a814bf680a56c3ce2442f2b3624409082bb7a9bb
Hazqeel <hazqeel9@gmail.com>
2023-10-29 17:16:22 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'ellzaf_ml',
+  packages = find_packages(),
+  version = '0.1.0',
+  license='MIT',
+  description = 'Ellzaf ML',
+  long_description_content_type = 'text/markdown',
+  author = 'Hazqeel Afyq',
+  author_email = 'hazqeel9@gmail.com',
+  url = 'https://github.com/Hazqeel09/ellzaf_ml',
+  keywords = [
+    'artificial intelligence',
+    'machine learning',
+  ],
+  install_requires=[
+    'einops>=0.7.0',
+    'torch>=1.10',
+    'torchvision'
+  ],
+  setup_requires=[
+    'pytest-runner',
+  ],
+  tests_require=[
+    'pytest',
+    'torch==1.12.1',
+    'torchvision==0.13.1'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
