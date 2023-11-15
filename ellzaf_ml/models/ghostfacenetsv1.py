7ffc94cda9a3c2717997aba71c38311e45b542b3
Hazqeel <hazqeel9@gmail.com>
2023-11-15 21:55:11 +0800

    fix gfnv1

--- ellzaf_ml/models/ghostfacenetsv1.py
+++ ellzaf_ml/models/ghostfacenetsv1.py
@@ -267,11 +267,6 @@ class GhostFaceNetsV1(nn.Module):
         x = self.bn1(x)
         x = self.act1(x)
         x = self.blocks(x)
-        x = self.global_pool(x)
-        x = self.conv_head(x)
-        x = self.act2(x)
-        x = x.view(x.size(0), -1)
-        if self.dropout > 0.:
-            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.pointwise_conv(x)
         x = self.classifier(x)
         return x
