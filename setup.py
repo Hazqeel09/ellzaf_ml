96f27ade165146089c3324d9ed4812d3ca4b367c
Hazqeel <hazqeel9@gmail.com>
2023-11-15 21:48:19 +0800

    fix gfnv1 and gfnv2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'ellzaf_ml',
   packages = find_packages(),
-  version = '1.1.0',
+  version = '1.1.1',
   license='MIT',
   description = 'Ellzaf ML',
   long_description_content_type = 'text/markdown',
