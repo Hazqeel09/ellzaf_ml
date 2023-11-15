0a361abf75e0763b588eb486c84ce60a03aed660
Hazqeel Afyq <33782405+Hazqeel09@users.noreply.github.com>
2023-11-15 15:25:31 +0800

    Update __init__.py

--- ellzaf_ml/models/__init__.py
+++ ellzaf_ml/models/__init__.py
@@ -1,4 +1,4 @@
-from .ghostfacenetsv2 import ghostfacenetsv2
+from .ghostfacenetsv2 import GhostFaceNetsV2
 from .lcff import LBPCNNFeatureFusion
 from .ldnet import LDnet
 from .spectformer import SpectFormer
