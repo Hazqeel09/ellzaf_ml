46e7dd264a8c02820d5e2edc39d5c5e921b955c9
Hazqeel <hazqeel9@gmail.com>
2024-02-23 21:38:14 +0800

    Revert "v1.3.4"
    This reverts commit c771b2a4b0ba5972ab58fcc9ccb5654d1b8bc0ea.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'ellzaf_ml',
   packages = find_packages(),
-  version = '1.3.4',
+  version = '1.3.3',
   license='MIT',
   description = 'Ellzaf ML',
   long_description_content_type = 'text/markdown',
