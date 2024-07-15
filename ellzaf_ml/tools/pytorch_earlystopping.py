1fd06fb2b2e67b1b01783d4e58bb5d58b0de9b73
Hazqeel Afyq <33782405+Hazqeel09@users.noreply.github.com>
2024-07-15 11:36:17 +0800

    feat: able to save two diff versions of model in early stopping

--- ellzaf_ml/tools/pytorch_earlystopping.py
+++ ellzaf_ml/tools/pytorch_earlystopping.py
@@ -3,7 +3,7 @@ import torch
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', ema_path='ema_checkpoint.pt', trace_func=print):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
@@ -14,6 +14,8 @@ class EarlyStopping:
                             Default: 0
             path (str): Path for the checkpoint to be saved to.
                             Default: 'checkpoint.pt'
+            ema_path (str): Path for the EMA checkpoint to be saved to.
+                            Default: 'ema_checkpoint.pt'
             trace_func (function): trace print function.
                             Default: print            
         """
@@ -25,14 +27,15 @@ class EarlyStopping:
         self.val_loss_min = np.Inf
         self.delta = delta
         self.path = path
+        self.ema_path = ema_path
         self.trace_func = trace_func
-    def __call__(self, val_loss, model):
 
+    def __call__(self, val_loss, model, ema_model=None):
         score = -val_loss
 
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            self.save_checkpoint(val_loss, model, ema_model)
         elif score <= self.best_score + self.delta:
             self.counter += 1
             self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
@@ -45,12 +48,14 @@ class EarlyStopping:
                 self.early_stop = True
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            self.save_checkpoint(val_loss, model, ema_model)
             self.counter = 0
 
-    def save_checkpoint(self, val_loss, model):
+    def save_checkpoint(self, val_loss, model, ema_model=None):
         '''Saves model when validation loss decrease.'''
         if self.verbose:
             self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
         torch.save(model.state_dict(), self.path)
+        if ema_model is not None:
+            torch.save(ema_model.state_dict(), self.ema_path)
         self.val_loss_min = val_loss
